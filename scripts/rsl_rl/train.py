888f136bd5585aebc0bef0e4562dd299c0306f4f
asa <96153+asa@users.noreply.github.com>
2026-08-08 10:45:49 -0700

    rsl_rl train: shim dump_pickle for IsaacLab versions without pickle IO

--- scripts/rsl_rl/train.py
+++ scripts/rsl_rl/train.py
@@ -83,7 +83,17 @@ from isaaclab.envs import (
     multi_agent_to_single_agent,
 )
 from isaaclab.utils.dict import print_dict
-from isaaclab.utils.io import dump_pickle, dump_yaml
+try:
+    from isaaclab.utils.io import dump_pickle, dump_yaml
+except ImportError:  # IsaacLab dropped pickle IO (utils.io >= 2.x)
+    import pickle
+
+    from isaaclab.utils.io import dump_yaml
+
+    def dump_pickle(filename, data):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "wb") as f:
+            pickle.dump(data, f)
 from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper
 from isaaclab_tasks.utils import get_checkpoint_path
 from isaaclab_tasks.utils.hydra import hydra_task_config
